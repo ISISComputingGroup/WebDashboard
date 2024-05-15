2936dab802c930d536960eaefb5db6c21371f456
Jack Harper <jack.harper@stfc.ac.uk>
2024-05-15 14:13:04 +0100

    make decompress api work

--- pages/api/decompress.js
+++ pages/api/decompress.js
@@ -1,9 +1,15 @@
+const util = require("util");
+const { exec } = require("child_process");
 
-  
   export default async function handler(req, res) {
-    const { data } = req.body;
+    const reqBuffer = Buffer(req.body);
 
-    const command = `C:\\Instrument\\Apps\\EPICS\\config_env.bat && uzhex`+ data;
+    if (!reqBuffer.length) {
+      res.status(500).json("Error: empty argument list")
+      return
+    }
+
+    const command = `C:\\Instrument\\Apps\\EPICS\\support\\utilities\\master\\bin\\windows-x64\\uzhex.exe ` + reqBuffer;
 
     // Promisify the exec function to use async/await
     const execAsync = util.promisify(exec);
@@ -13,17 +19,19 @@
       const { stdout, stderr } = await execAsync(command);
 
       // Check for any errors
-      if (stderr) {
+      if (stderr ) {
         console.error(`Error: ${stderr}`);
-        res.status(500); // or throw an error, depending on your use case
+        res.status(500).json(`error: ${stderr}`); // or throw an error, depending on your use case
+        return
+      }  else if (stdout == null) {
+        res.status(500).json(`error: command returned no output`); // or throw an error, depending on your use case
+        return
       }
       res.status(200).json(stdout);
     } catch (error) {
       console.error("Error:", error.message);
-      res.status(500); // or throw an error, depending on your use case
+      res.status(500).json(error.message); // or throw an error, depending on your use case
+      return
     }
-
-
-    
   }
   
