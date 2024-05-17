da69a1eee748e0b0cdd86cf565538da42e4ed060
Jack Harper <jack.harper@stfc.ac.uk>
2024-05-17 11:40:06 +0100

    top bar getting pvs now

--- components/InstrumentData.js
+++ components/InstrumentData.js
@@ -3,10 +3,11 @@ import TopBar from "./TopBar";
 import Groups from "./Groups";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useWebSocket from "react-use-websocket";
 
 export default function InstrumentData() {
   // set up the different states for the instrument data
-  const [socket, setSocket] = useState(null);
+  // const [socket, setSocket] = useState(null);
 
   const router = useRouter();
   const [slug, setSlug] = useState(null);
@@ -22,90 +23,183 @@ export default function InstrumentData() {
 
 
 
+  // useEffect(() => {
+
+
+  //   // sub to config PV
+    
+  //   // based on config PV reload/set group/block data array and subscriptions
+
+  //   //permanent subscriptions
+  //   //dae shit
+  //   //blocks
+
+  //   //adhoc subscriptions
+  //   //dictionary of pv names for blocks etc -> websocket last data
+
+
+  
+    
+  // }, [router.query.slug]);
+
+  const [monitoredPVs, setMonitoredPVs] = useState([]);
+
+
   useEffect(() => {
-    if (!router.query.slug) {
+
+    if (!router.query.slug || !router.query.slug[0] ) {
       return;
     }
     setSlug(router.query.slug);
     console.log(router.query.slug);
     setInstName(router.query.slug[0]);
 
-    // sub to config PV
-    
-    // based on config PV reload/set group/block data array and subscriptions
+      // prefix - TODO move this to function - will not work on dev machines, long named insts/setup machines etc. 
+  const instrument_name_upper = router.query.slug[0].toUpperCase();
+
+  const prefix = `IN:${instrument_name_upper}:`;
+
+  const CONFIG_DETAILS = "CS:BLOCKSERVER:GET_CURR_CONFIG_DETAILS";
+  
+  const perm_pvs = [
+    `${prefix}${CONFIG_DETAILS}`,
+    `${prefix}DAE:RUNSTATE`,
+    "sim://sine:"
+    // all the other dae/topbar pvs here
+  ]
+
+
+  const topBarMap = new Map(
+    Object.entries({
+      [`${prefix}DAE:RUNSTATE`]: "Run state",
+      [`${prefix}DAE:RUNNUMBER`]: "Run number",
+      [`${prefix}DAE:STARTTIME`]: "Start number",
+      [`${prefix}DAE:TITLE`]: "Title",
+      [`${prefix}DAE:TITLE:DISPLAY`]: "",
+      [`${prefix}DAE:GOODFRAMES`]: "",
+      [`${prefix}DAE:GOODFRAMES_PD`]: "",
+      [`${prefix}DAE:MONITORCOUNTS`]: "",
+      [`${prefix}DAE:RUNDURATION`]: "",
+      [`${prefix}DAE:RUNDURATION_PD`]: "",
+      [`${prefix}DAE:_USERNAME`]: "",
+      [`${prefix}DAE:_USERNAME:SP`]: "",
+      [`${prefix}DAE:_USERNAME:SP:RBV`]: "",
+      [`${prefix}DAE:RAWFRAMES`]: "",
+      [`${prefix}DAE:RAWFRAMES_PD`]: "",
+      [`${prefix}DAE:PERIOD`]: "",
+      [`${prefix}DAE:NUMPERIODS`]: "",
+      [`${prefix}DAE:GOODUAH`]: "",
+      [`${prefix}DAE:GOODUAH_PD`]: "",
+      [`${prefix}DAE:COUNTRATE`]: "",
+      [`${prefix}DAE:NPRATIO`]: "",
+      [`${prefix}DAE:DAETIMINGSOURCE`]: "",
+      [`${prefix}DAE:PERIODTYPE`]: "",
+      [`${prefix}DAE:NUMTIMECHANNELS`]: "",
+      [`${prefix}DAE:DAEMEMORYUSED`]: "",
+      [`${prefix}DAE:NUMSPECTRA`]: "", 
+      [`${prefix}DAE:MONITORCOUNTS`]: "",
+      [`${prefix}DAE:PERIODSEQ`]: "",
+      [`${prefix}DAE:BEAMCURRENT`]: "",
+      [`${prefix}DAE:TOTALUAMPS`]: "",
+      [`${prefix}DAE:MEVENTS`]: "",
+      [`${prefix}DAE:TOTALDAECOUNTS`]: "",
+      [`${prefix}DAE:EVENTMODEFRACTION`]: "",
+      [`${prefix}DAE:EVENTMODEBUFUSED`]: "",
+      [`${prefix}DAE:MONITORSPECTRUM`]: "",
+      [`${prefix}DAE:MONITORFROM`]: "",
+      [`${prefix}DAE:MONITORTO`]: "",
+      [`${prefix}DAE:NUMSPECTRA`]: "",
+      [`${prefix}DAE:NUMTIMECHANNELS`]: "",
+      [`${prefix}DAE:RUNSTATE_STR`]: "",
+      [`${prefix}DAE:STATETRANS`]: "",
+      [`${prefix}DAE:STATE:CHANGING`]: "",
+      [`${prefix}DAE:STATETRANS:TIME`]: "",
+      [`${prefix}DAE:VETOSTATUS`]: "",
+      [`${prefix}DAE:VETOPC`]: "",
+      [`sim://sine`]: "",
+
+    })
+  );
+
+
+  for (const pv of topBarMap.keys()) {
+    console.log("subscribing to " + pv)
+    sendJsonMessage({ "type": "subscribe", "pvs": [pv] })
+
+  }
+
+  }, [router.query.slug, sendJsonMessage])
+
+  useEffect(() => {
+   
+  console.log(lastJsonMessage)
+    if (lastJsonMessage !== null) {
+      console.log(lastJsonMessage)
+        const updatedPV = lastJsonMessage;
+        const updatedPVName = updatedPV.pv;
+        let pvVal;
+        if (updatedPV.value == null) {
+            pvVal = updatedPV.text
+        }
+         else {
+        pvVal = updatedPV.value
+
+
+    }
+    // if (updatedPV.pv == `${prefix}${CONFIG_DETAILS}` && updatedPV.text != null){
+    //     let raw = updatedPV.text;
+
+        
+    //     //TODO send API request to decompress here
+    //     //TODO clear existing array for blocks
+    //     //TODO reset subscriptions and setup new ones
+
+
+    // }
 
     
-
-    //permanent subscriptions
-    //dae shit
-    //blocks
-
-    //adhoc subscriptions
-    //dictionary of pv names for blocks etc -> websocket last data
-
-
-    
-    // prefix - TODO move this to function
-    const prefix = `IN:${this.inst_name.toUpperCase()}:`
-
-    const CONFIG_DETAILS = "CS:BLOCKSERVER:GET_CURR_CONFIG_DETAILS";
-    
-    const perm_pvs = [
-      `${prefix}${CONFIG_DETAILS}`,
-      `${prefix}DAE:RUNSTATE`,
-      // all the other dae/topbar pvs here
-    ]
-
-
-    sendJsonMessage({ "type": "subscribe", "pvs": perm_pvs })
-
-    useEffect(() => {
-      if (lastJsonMessage !== null) {
-          const updatedPV = lastJsonMessage;
-          const updatedPVName = updatedPV.pv;
-          let pvVal;
-          if (updatedPV.value == null) {
-              pvVal = updatedPV.text
-          }
-           else {
-          pvVal = updatedPV.value
-      }
-      if (updatedPV.pv == `${prefix}${CONFIG_DETAILS}` && updatedPV.text != null){
-          let raw = updatedPV.text;
-
-          
-          //TODO send API request to decompress here
-          //TODO clear existing array for blocks
-          //TODO reset subscriptions and setup new ones
-
-
-      }
-
-      setMonitoredPVs(prevMonitoredPVs => ({
+    setMonitoredPVs(prevMonitoredPVs => ({
           ...prevMonitoredPVs,
           [updatedPVName]: pvVal
       }));
-          }
-  }, [lastJsonMessage])
+
+      const pv = document.getElementById(updatedPVName + "_VALUE");
+
+      if (!pv) return;
+  
+      // if pv already has a green bg, dont do anything
+      if (pv.classList.contains("text-green-500")) return;
+      pv.classList.remove("text-transparent");
+      pv.classList.add(
+        // "bg-green-500",
+        // "transition-all",
+        "text-green-500"
+        // "duration-1000",
+        // "ease-in-out",
+        // "font-bold"
+      );
+  
+      setTimeout(() => {
+        // pv.classList.remove("bg-green-500");
+        pv.classList.remove("text-green-500");
+        pv.classList.add("text-transparent");
+      }, 2000);
+
+    // setMonitoredPVs(prevMonitoredPVs => ({
+    //     ...prevMonitoredPVs,
+    //     [updatedPVName]: pvVal
+    // }));
+        }
+}, [ sendJsonMessage, lastJsonMessage])
 
 
-
-
-      
-
-
-    return () => {
-      socket?.disconnect();
-    };
-  }, [router.query.slug]);
-
-  if (!socket || !slug || !instName) {
+  if ( !slug || !instName) {
     return <h1>Loading...</h1>;
   }
   return (
     <div className="p-8 w-full mx-auto max-w-7xl">
-      <TopBar socket={socket} instName={instName} />
-      <Groups socket={socket} />
+      <TopBar monitoredPVs={monitoredPVs} instName={instName}/>
+      <Groups />
     </div>
   );
 }
--- components/TopBar.js
+++ components/TopBar.js
@@ -1,119 +1,13 @@
 import React, { useState, useCallback, useEffect } from "react";
 import io from "socket.io-client";
 
-const TopBar = ({ socket, instName }) => {
-  // socket work
-  const [instData, setInstData] = useState({});
+const TopBar = ({ monitoredPVs, instName }) => {
 
-  const updateInstData = async (data) => {
-    await setInstData((prevData) => {
-      return {
-        ...prevData,
-        [data["pvName"]]: data,
-      };
-    });
+  console.log(monitoredPVs)
+  console.log(instName)
 
-    // find the pv with its id and make bg greenf or 2 seconds
-    const pv = document.getElementById(data["pvName"] + "_VALUE");
-
-    if (!pv) return;
-
-    // if pv already has a green bg, dont do anything
-    if (pv.classList.contains("text-green-500")) return;
-    pv.classList.remove("text-transparent");
-    pv.classList.add(
-      // "bg-green-500",
-      // "transition-all",
-      "text-green-500"
-      // "duration-1000",
-      // "ease-in-out",
-      // "font-bold"
-    );
-
-    setTimeout(() => {
-      // pv.classList.remove("bg-green-500");
-      pv.classList.remove("text-green-500");
-      pv.classList.add("text-transparent");
-    }, 2000);
-  };
-
-  useEffect(() => {
-    if (!socket) return;
-
-    socket.on("instData", (data) => {
-      // console.log("InstData received:", data);
-      updateInstData(data);
-    });
-
-    return () => {
-      socket.off("connect");
-      socket.off("instData");
-    };
-  }, [socket]);
-
-  // template work
-  // const [search, setSearch] = useState("");
-  // const [search2, setSearch2] = useState("");
-  // const [search3, setSearch3] = useState("");
-
-  // const handleSearch = (event) => {
-  //   setSearch(event.target.value);
-  // };
-  // const handleSearch2 = (event) => {
-  //   setSearch2(event.target.value);
-  // };
-  // const handleSearch3 = (event) => {
-  //   setSearch3(event.target.value);
-  // };
-
-  // const [filteredPv1, setFilteredPv1] = useState([]);
-  // const [filteredPv2, setFilteredPv2] = useState([]);
-  // const [filteredPv3, setFilteredPv3] = useState([]);
-
-  // // create some pvs just so we can see the tables etc and develop the page
-  // useEffect(() => {
-  //   const pvs = Object.entries(instPvs);
-  //   const pvLength = pvs.length;
-  //   // Example of item in a pvSet:
-  //   // [
-  //   //     "RUNSTATE",
-  //   //     {
-  //   //         "status": "Connected",
-  //   //         "value": 1,
-  //   //         "alarm": "",
-  //   //         "visibility": true,
-  //   //         "rc_enabled": "NO"
-  //   //     }
-  //   // ],
-  //   const pvSet1 = instData.slice(0, Math.floor(pvLength / 3));
-  //   const pvSet2 = instData.slice(
-  //     Math.floor(pvLength / 3),
-  //     Math.floor((pvLength / 3) * 2)
-  //   );
-  //   const pvSet3 = instData.slice(Math.floor((pvLength / 3) * 2), pvLength);
-
-  //   // search functionality, will be split into components later to not have this repetition
-
-  //   setFilteredPv1(
-  //     pvSet1.filter(([block]) => {
-  //       return block.toLowerCase().includes(search.toLowerCase());
-  //     })
-  //   );
-
-  //   setFilteredPv2(
-  //     pvSet2.filter(([block]) => {
-  //       return block.toLowerCase().includes(search2.toLowerCase());
-  //     })
-  //   );
-  //   setFilteredPv3(
-  //     pvSet3.filter(([block]) => {
-  //       return block.toLowerCase().includes(search2.toLowerCase());
-  //     })
-  //   );
-  // }, []);
-
-  if (!socket || !instData) {
-    return <LoadingSkeleton />;
+  if (!monitoredPVs) {
+    return "";
   }
 
   return (
@@ -139,8 +33,8 @@ const TopBar = ({ socket, instName }) => {
         >
           {instName.toUpperCase()} is{" "}
           <span>
-            {instData["IN:" + instName.toUpperCase() + ":DAE:RUNSTATE"] &&
-              instData["IN:" + instName.toUpperCase() + ":DAE:RUNSTATE"][
+            {monitoredPVs["IN:" + instName.toUpperCase() + ":DAE:RUNSTATE"] &&
+              monitoredPVs["IN:" + instName.toUpperCase() + ":DAE:RUNSTATE"][
                 "value"
               ]}
           </span>
@@ -158,7 +52,7 @@ const TopBar = ({ socket, instName }) => {
               </tr>
             </thead>
             <tbody className="text-gray-200 ">
-              {Object.keys(instData).map((pv) => (
+              {Object.keys(monitoredPVs).map((pv) => (
                 <tr
                   id={pv}
                   key={pv}
@@ -166,7 +60,7 @@ const TopBar = ({ socket, instName }) => {
                 >
                   <td className="py-1 px-4">{pv}</td>
                   <td className="py-1 px-4 flex justify-between items-center">
-                    <span>{instData[pv]["value"]}</span>
+                    <span>{monitoredPVs[pv]}</span>
                     <svg
                       id={pv + "_VALUE"}
                       className="min-w-2 min-h-2 max-w-2 max-h-2 transition-all text-transparent"
@@ -182,165 +76,7 @@ const TopBar = ({ socket, instName }) => {
             </tbody>
           </table>
         </div>
-        {/* </div>
-      <div className="flex-col flex items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 overflow-x-auto   w-full">
-          <div className="bg-gray-50 border-2 border-gray-800 m-4 p-4 shadow-md ">
-            <h2 className=" font-semibold text-xl ">Soft</h2>
-
-            <div className="relative my">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                onChange={handleSearch}
-                type="search"
-                value={search}
-                id="search"
-                autoComplete="off"
-                className="block w-full p-2 ps-10 text-sm text-gray-900  bg-gray-100   "
-                placeholder="Search for PV name"
-              />
-            </div>
-            <table className="text-sm w-full table-fixed ">
-              <thead>
-                <tr className="bg-blue-gray-100 text-gray-700">
-                  <th className="py-3  text-left">Name</th>
-                  <th className="py-3 text-left">Value</th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-200 ">
-                {filteredPv1.map((block) => (
-                  <tr
-                    key={block[0]}
-                    className="border-b border-gray-300 text-black transition duration-100 hover:bg-gray-700 hover:text-white"
-                  >
-                    <td className="py-1">{block[0]}</td>
-                    <td className="py-1 ">{block[1]["value"]}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <div className="bg-gray-50 border-2 border-gray-800 m-4 p-4 shadow-md ">
-            <h2 className=" font-semibold text-xl ">Technical</h2>
-
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                onChange={handleSearch2}
-                type="search"
-                value={search2}
-                id="search2"
-                autoComplete="off"
-                className="block w-full p-2 ps-10 text-sm text-gray-900  bg-gray-100 focus:ring-blue-500  "
-                placeholder="Search for PV name"
-              />
-            </div>
-            <table className="text-sm w-full table-fixed ">
-              <thead>
-                <tr className="bg-blue-gray-100 text-gray-700">
-                  <th className="py-3  text-left">Name</th>
-                  <th className="py-3  text-left">Value</th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-200 ">
-                {filteredPv2.map((block) => (
-                  <tr
-                    key={block[0]}
-                    className="border-b border-gray-300 text-black transition duration-100 hover:bg-gray-700 hover:text-white"
-                  >
-                    <td className="py-1 ">{block[0]}</td>
-                    <td className="py-1 ">{block[1]["value"]}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <div className="bg-gray-50 border-2 border-gray-800 m-4 p-4 shadow-md ">
-            <h2 className=" font-semibold text-xl ">Time</h2>
-
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                onChange={handleSearch3}
-                type="search"
-                value={search3}
-                id="search3"
-                autoComplete="off"
-                className="block w-full p-2 ps-10 text-sm text-gray-900  bg-gray-100 focus:ring-blue-500  "
-                placeholder="Search for PV name"
-              />
-            </div>
-            <table className="text-sm w-full table-fixed ">
-              <thead>
-                <tr className="bg-blue-gray-100 text-gray-700">
-                  <th className="py-3  text-left">Name</th>
-                  <th className="py-3  text-left">Value</th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-200 ">
-                {filteredPv3.map((block) => (
-                  <tr
-                    key={block[0]}
-                    className="border-b border-gray-300 text-black transition duration-100 hover:bg-gray-700 hover:text-white"
-                  >
-                    <td className="py-1 ">{block[0]}</td>
-                    <td className="py-1 ">{block[1]["value"]}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div> */}
+        {}
       </div>
     </div>
   );
