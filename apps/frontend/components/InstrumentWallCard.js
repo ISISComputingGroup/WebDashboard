d18283e7b7a7a0c97a06336763d3c71225857735
Jack Harper <jack.harper@stfc.ac.uk>
2024-05-22 23:15:18 +0100

    add some missing colours

--- apps/frontend/components/InstrumentWallCard.js
+++ apps/frontend/components/InstrumentWallCard.js
@@ -4,7 +4,13 @@ import { motion } from "framer-motion";
 function getStatusColor(status) {
   switch (status) {
     case "UNKNOWN":
+      return "bg-yellow-500";
+    case "PAUSED":
       return "bg-red-500";
+    case "PAUSING":
+      return "bg-red-950";
+    case "RESUMING":
+      return "bg-green-900";
     case "RUNNING":
       return "bg-[#90EE90]";
     case "BEGINNING":
@@ -27,6 +33,12 @@ function getStatusColor(status) {
 function getForegroundColor(status) {
   switch (status) {
     case "UNKNOWN":
+      return "text-white";
+    case "PAUSED":
+      return "text-white";
+    case "PAUSING":
+      return "text-white";        
+    case "RESUMING":
       return "text-white";
     case "RUNNING":
       return "text-white";
