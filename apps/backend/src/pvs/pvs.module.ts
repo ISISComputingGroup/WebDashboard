2bcc76189b3a8de1096da17e1925d6c7d1607ac3
Jack Harper <jack.harper@stfc.ac.uk>
2024-07-03 10:30:32 +0100

    remove backend

--- apps/backend/src/pvs/pvs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PvsController } from './pvs.controller';
-import { PvsService } from './pvs.service';
-
-@Module({
-  controllers: [PvsController],
-  providers: [PvsService],
-})
-export class PvsModule {}
