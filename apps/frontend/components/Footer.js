fed5423cd673da4c908ca2c48e5763ffeedac9aa
Jack Harper <jack.harper@stfc.ac.uk>
2024-07-03 12:33:14 +0100

    set up jest for testing, add basic test

--- apps/frontend/components/Footer.js
+++ apps/frontend/components/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <section className="bg-white dark:bg-zinc-900  ">
       <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8 w-full flex flex-col justify-center items-center ">
         <Link href="/" className="flex items-center justify-center ">
-          <img src="/ibex_logo.png" alt="IBEX Logo" className="h-16 w-auto" />
+          <Image src="/ibex_logo.png" alt="IBEX Logo" className="h-16 w-auto" width={128} height={128} />
         </Link>
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <Link
