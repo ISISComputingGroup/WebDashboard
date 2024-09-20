2f303936890033e4ad92e1ba0786017e9fa8c695
Jack Harper <jack.harper@stfc.ac.uk>
2024-09-20 17:26:19 +0100

    review comments - css

--- app/wall/page.tsx
+++ app/wall/page.tsx
@@ -166,8 +166,8 @@ export default function WallDisplay() {
                 className="peer/showLabel absolute scale-0"
                 type="checkbox"
               />
-              <span className="block max-h-14 overflow-hidden rounded-lg hover:bg-gray-800 px-4 py-0 mb-2  shadow-lg transition-all duration-300 peer-checked/showLabel:max-h-fit cursor-pointer">
-                <h3 className="flex h-14 cursor-pointer items-center font-bold justify-center ">
+              <span className="block max-h-14 overflow-hidden rounded-lg bg-zinc-600 hover:bg-gray-800 px-4 py-0 mb-2  shadow-lg transition-all duration-300 peer-checked/showLabel:max-h-fit cursor-pointer">
+                <h3 className="flex h-14 cursor-pointer items-center font-bold justify-center text-white ">
                   Show/hide beam info
                 </h3>
                 <Image
