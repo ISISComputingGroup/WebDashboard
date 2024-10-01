823ae5f4d1ebef29ff98658d454ac3d0355fdee3
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-01 11:07:03 +0100

    hmm, use suspense

--- app/instrument/page.tsx
+++ app/instrument/page.tsx
@@ -3,16 +3,25 @@ import { Inter } from "next/font/google";
 import InstrumentData from "@/app/components/InstrumentData";
 const inter = Inter({ subsets: ["latin"] });
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-
-export default function Instrument() {
+function InstrumentPage() {
     const searchParams = useSearchParams()
     const instrument = searchParams.get("name")!;
+
+    return       <InstrumentData instrumentName={instrument} />
+
+}
+
+export default function Instrument() {
+
   return (
     <main
       className={`flex min-h-screen bg-white flex-col items-center justify-start  ${inter.className}`}
     >
-      <InstrumentData instrumentName={instrument} />
+        <Suspense>
+        <InstrumentPage />
+      </Suspense>
     </main>
   );
 }
