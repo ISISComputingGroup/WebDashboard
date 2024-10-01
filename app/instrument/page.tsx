62204c3e5b902af44ec15278628981b378e241fd
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-01 11:15:31 +0100

    format

--- app/instrument/page.tsx
+++ app/instrument/page.tsx
@@ -1,25 +1,23 @@
-'use client'
+"use client";
 import { Inter } from "next/font/google";
 import InstrumentData from "@/app/components/InstrumentData";
 const inter = Inter({ subsets: ["latin"] });
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function InstrumentPage() {
-    const searchParams = useSearchParams()
-    const instrument = searchParams.get("name")!;
+  const searchParams = useSearchParams();
+  const instrument = searchParams.get("name")!;
 
-    return       <InstrumentData instrumentName={instrument} />
-
+  return <InstrumentData instrumentName={instrument} />;
 }
 
 export default function Instrument() {
-
   return (
     <main
       className={`flex min-h-screen bg-white flex-col items-center justify-start  ${inter.className}`}
     >
-        <Suspense>
+      <Suspense>
         <InstrumentPage />
       </Suspense>
     </main>
