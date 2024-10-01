a742dd6dbe433ba968709fa863e214a67963c69a
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-01 11:41:28 +0100

    use process.env.basepath

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   output: "standalone",
-  basePath: "/WebDashboard",
+  basePath: process.env.BASE_PATH,
   images: {
     remotePatterns: [
       {
