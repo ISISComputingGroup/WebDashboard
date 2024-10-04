d69e9615ddf91af29a7aeaa558001a3cb8967b9f
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-10-04 13:52:30 +0100

    Undo accidental changes

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import InstList from "@/app/components/InstList";
 const inter = Inter({ subsets: ["latin"] });
-import Head from 'next/head';
 
 export default function Home() {
   let instList = InstList();
