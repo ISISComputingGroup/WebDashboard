a6ebe425e1e5ec1a425b4fdec9d08ed5918e0fa1
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-29 14:29:37 +0000

    get coverage working

---
+++ app/components/IfcPV.test.ts
@@ -0,0 +1,36 @@
+import {findPVByAddress, IfcPV} from "@/app/components/IfcPV";
+
+test("findPVByAddress finds a PV and returns it", () => {
+    const pvAddressToTest = "SOME:PV"
+    const myPVs: Array<IfcPV> = [
+        {pvaddress: pvAddressToTest}, {pvaddress:"SOME:OTHER:PV"}
+    ]
+    const result = findPVByAddress(myPVs, pvAddressToTest);
+
+    expect(result?.pvaddress).toBe(pvAddressToTest);
+});
+
+
+test("findPVByAddress does not find a nonexistant PV and returns undefined", () => {
+    const pvAddressToTest = "SOME:PV"
+    const myPVs: Array<IfcPV> = [
+        {pvaddress:"THE:ONLY:PV"}
+    ]
+    const result = findPVByAddress(myPVs, pvAddressToTest);
+
+    expect(result).toBe(undefined);
+});
+
+// test("findPVByHumanReadableName finds a PV and returns it", () => {
+//     const prefix = "UNITTESTING"
+//
+//     expect(result).toBe(undefined);
+// });
+//
+//
+// test("findPVByHumanReadableName does not find a nonexistant PV and returns undefined", () => {
+//     const prefix = "UNITTESTING"
+//
+//     expect(result).toBe(undefined);
+// });
+
--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+  ]
 });
 
 // Add any custom config to be passed to Jest
