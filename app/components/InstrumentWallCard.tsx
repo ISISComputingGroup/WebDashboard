239a98b720a4cc28128a614f01607e1a99e0f0c6
Jack Harper <jack.harper@stfc.ac.uk>
2025-02-19 10:36:37 +0000

    make instruments more like buttons

--- app/components/InstrumentWallCard.tsx
+++ app/components/InstrumentWallCard.tsx
@@ -9,19 +9,17 @@ export default function InstrumentWallCard({
   instrument: IfcInstrumentStatus;
 }) {
   return (
-    <div
-      className={`flex items-center justify-between p-3  rounded-lg shadow-sm border-2 border-gray-700 dark:border-gray-200 hover:shadow-lg hover:border-black dark:hover:border-gray-700 transition-all duration-200
+    <div className={"flex"}>
+      <Link
+        href={"/instrument?name=" + instrument.name}
+        target="_blank"
+        className={`flex items-center justify-center text-center p-3 min-w-28 w-auto max-h-12 rounded-lg shadow-sm border-2 border-gray-700 dark:border-gray-200 hover:shadow-lg hover:border-black dark:hover:border-gray-700 transition-all duration-200
       ${getStatusColour(instrument.runstate || "UNKNOWN")} ${getForegroundColour(
         instrument.runstate || "UNKNOWN",
       )}`}
-    >
-      <Link
-        href={"/instrument?name=" + instrument.name}
-        className="flex items-center justify-center  lg:w-20 w-full h-4"
-        target="_blank"
       >
-        <div className="flex flex-col justify-center items-center">
-          <span className="text-sm font-bold truncate line-clamp-1 ">
+        <div className="flex flex-col">
+          <span className="text-sm font-bold line-clamp-1 w-full">
             {instrument.name}
           </span>
           <span className="text-xs ">{instrument.runstate || "UNKNOWN"}</span>
