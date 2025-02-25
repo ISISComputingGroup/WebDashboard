e2437115687e5c6cceadb6b4ca6129c6bf7e1fdb
Jack Harper <jack.harper@stfc.ac.uk>
2025-02-25 20:08:27 +0000

    sort wall display by name, sciencegroup already does this

--- app/components/TargetStation.tsx
+++ app/components/TargetStation.tsx
@@ -11,6 +11,9 @@ export default function TargetStation({
   instruments: Array<instListEntryWithRunstatePVandValue>;
   beamCurrent: number | null | undefined;
 }) {
+  instruments = instruments.sort((instrumenta, instrumentb) =>
+    instrumenta.name.localeCompare(instrumentb.name),
+  );
   return (
     <div className="flex flex-col justify-center items-start w-full">
       <h1 className="w-full text-left text-gray-600 dark:text-gray-200 font-semibold text-md mt-2 py-2 ">
