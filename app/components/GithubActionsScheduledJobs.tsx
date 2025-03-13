ef37c361ac1d22964c1defff2767785127cab7ac
Jack Harper <jack.harper@stfc.ac.uk>
2025-03-13 18:53:57 +0000

    pull out into component and make prettier

--- app/components/GithubActionsScheduledJobs.tsx
+++ app/components/GithubActionsScheduledJobs.tsx
@@ -1,39 +1,44 @@
-import Image from "next/image";
+function GithubActionsJob({
+  repo,
+  workflowName,
+}: {
+  repo: string;
+  workflowName: string;
+}) {
+  return (
+    <div>
+      <a
+        href={`https://github.com/ISISComputingGroup/${repo}/actions/workflows/${workflowName}`}
+        className={
+          "flex-row flex items-center content-center h-10 w-full text-center rounded-lg bg-gray-800 border-2 border-black hover:border-white"
+        }
+      >
+        <h2 className={"text-center text-lg w-1/2"}>{repo}: </h2>
+        <img
+          src={`https://github.com/ISISComputingGroup/${repo}/actions/workflows/${workflowName}/badge.svg`}
+          alt={"ibex_bluesky_core lint-and-test-nightly badge"}
+          className={"w-1/2 pr-2"}
+        />
+      </a>
+    </div>
+  );
+}
 
 export default function GithubActionsScheduledJobs() {
   return (
-      <div className={"flex gap-4 dark:text-white"}>
-
-
-              <div>
-                  <h1>ibex_bluesky_core</h1>
-                  <a
-                      href={
-                          "https://github.com/ISISComputingGroup/ibex_bluesky_core/actions/workflows/lint-and-test-nightly.yml"
-                      }
-                  >
-                      <Image
-                          src="https://github.com/ISISComputingGroup/ibex_bluesky_core/actions/workflows/lint-and-test-nightly.yml/badge.svg"
-                          alt={"ibex_bluesky_core lint-and-test-nightly badge"} height={240} width={240}
-                      />
-                  </a>
-              </div>
-
-              <div>
-                  <h1>inst_servers</h1>
-                  <a
-                      href={
-                          "https://github.com/ISISComputingGroup/EPICS-inst_servers/actions/workflows/lint-and-test-nightly.yml"
-                      }
-                  >
-                      <Image
-                          src="https://github.com/ISISComputingGroup/EPICS-inst_servers/actions/workflows/lint-and-test-nightly.yml/badge.svg"
-                          alt={"inst_servers lint-and-test-nightly badge"} height={240} width={240}
-                      />
-                  </a>
-              </div>
-
-
-      </div>
+    <div
+      className={
+        "grid   md:grid-cols-3 items-center gap-1.5 grid-cols-1 dark:text-white"
+      }
+    >
+      <GithubActionsJob
+        repo={"ibex_bluesky_core"}
+        workflowName={"lint-and-test-nightly.yml"}
+      />
+      <GithubActionsJob
+        repo={"EPICS-inst_servers"}
+        workflowName={"lint-and-test-nightly.yml"}
+      />
+    </div>
   );
 }
--- app/wall/page.tsx
+++ app/wall/page.tsx
@@ -9,23 +9,22 @@ export default function WallDisplay() {
     >
       <section className="rounded-xl w-full md:px-0 md:w-11/12 my-2">
         <div className="mx-auto  ">
-          <div
-              className="w-full mx-auto text-left flex justify-center items-center pb-8 px-8 dark:bg-zinc-900 rounded-xl">
+          <div className="w-full mx-auto text-left flex justify-center items-center pb-8 px-8 dark:bg-zinc-900 rounded-xl">
             <div
-                id="status"
-                className="flex flex-col justify-center items-center"
+              id="status"
+              className="flex flex-col justify-center items-center"
             >
-              <InstrumentsDisplay/>
+              <InstrumentsDisplay />
             </div>
           </div>
-          <h1 className="w-full text-left text-black dark:text-white font-semibold text-2xl p-2 ">
+          <h1 className="w-full text-left text-black dark:text-white font-semibold text-2xl p-2">
             Jenkins jobs:
           </h1>
-          <JenkinsJobs/>
-          <h1 className="w-full text-left text-black dark:text-white font-semibold text-2xl p-2 ">
+          <JenkinsJobs />
+          <h1 className="w-full text-left text-black dark:text-white font-semibold text-2xl p-2">
             Github actions scheduled workflows:
           </h1>
-          <GithubActionsScheduledJobs/>
+          <GithubActionsScheduledJobs />
         </div>
       </section>
     </main>
