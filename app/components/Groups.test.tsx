78d65a76f972c9736ab9a6e75e32d168d53895e6
Jack Harper <jack.harper@stfc.ac.uk>
2025-05-14 09:56:07 +0100

    fix groups test

--- app/components/Groups.test.tsx
+++ app/components/Groups.test.tsx
@@ -1,40 +1,52 @@
 import { render } from "@testing-library/react";
 import Groups from "@/app/components/Groups";
-import { IfcGroup } from "@/app/types";
+import { tGroups } from "@/app/types";
 
 it("renders groups correctly with hidden and non hidden groups", () => {
-  const shownGroupWithTwoBlocks: IfcGroup = {
-    name: "group1",
-    blocks: [
-      {
-        pvaddress: "A:SHOWN:BLOCK",
-        human_readable_name: "aShownBlock",
-        value: 2.344,
-        visible: true,
-      },
-      {
-        pvaddress: "ADIFF:SHOWN:BLOCK",
-        human_readable_name: "aDifferentShownBlock",
-        value: 3.14,
-        visible: true,
-      },
-    ],
-  };
-  const hiddenGroupWithOneBlock: IfcGroup = {
-    name: "group2",
-    blocks: [
-      {
-        pvaddress: "A:HIDDEN:BLOCK",
-        human_readable_name: "aHiddenBlock",
-        visible: false,
-      },
-    ],
-  };
+  const groupsWithOneGroupThatHasAHiddenBlock: tGroups = new Map();
+
+  groupsWithOneGroupThatHasAHiddenBlock.set(
+    "group1",
+    new Map([
+      [
+        "A:SHOWN:BLOCK",
+        {
+          pvaddress: "A:SHOWN:BLOCK",
+          human_readable_name: "aShownBlock",
+          value: 2.344,
+          visible: true,
+        },
+      ],
+      [
+        "ADIFF:SHOWN:BLOCK",
+        {
+          pvaddress: "ADIFF:SHOWN:BLOCK",
+          human_readable_name: "aDifferentShownBlock",
+          value: 3.14,
+          visible: true,
+        },
+      ],
+    ]),
+  );
+  groupsWithOneGroupThatHasAHiddenBlock.set(
+    "group2",
+    new Map([
+      [
+        "A:HIDDEN:BLOCK",
+        {
+          pvaddress: "A:HIDDEN:BLOCK",
+          human_readable_name: "aHiddenBlock",
+          visible: false,
+        },
+      ],
+    ]),
+  );
+
   const { container } = render(
     <Groups
       instName={"TESTING"}
       showHiddenBlocks={false}
-      groupsMap={[shownGroupWithTwoBlocks, hiddenGroupWithOneBlock]}
+      groupsMap={groupsWithOneGroupThatHasAHiddenBlock}
     />,
   );
   expect(container).toMatchSnapshot();
