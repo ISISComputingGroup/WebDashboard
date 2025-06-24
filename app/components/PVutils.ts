d7e41576ea4834c1ddd8cc64db1e6dea2894df7a
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-06-24 18:04:45 +0100

    Update PVutils.ts

--- app/components/PVutils.ts
+++ app/components/PVutils.ts
@@ -8,7 +8,7 @@ export function ExponentialOnThresholdFormat(
   value: string | number,
   precision: number = 3,
 ) {
-  const nValue: number = (value == undefined || val == null) ? NaN : +value;
+  const nValue: number = (value == undefined || value == null) ? NaN : +value;
   if (isNaN(nValue)) {
     return value;
   } else {
