e56896efd382432e39f79631ca810e233f39776c
Jack Harper <jack.harper@stfc.ac.uk>
2025-12-08 11:27:56 +0000

    make the wall display festive

--- app/wall/page.tsx
+++ app/wall/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Snowfall from "react-snowfall";
 import GithubActionsScheduledJobs from "@/app/components/GithubActionsScheduledJobs";
 import InstrumentsDisplay from "@/app/components/InstrumentsDisplay";
 import JenkinsJobs from "../components/JenkinsJobs";
@@ -7,6 +9,7 @@ export default function WallDisplay() {
     <main
       className={`flex min-h-screen bg-white dark:bg-zinc-800 flex-col items-center justify-between`}
     >
+      <Snowfall />
       <section className="rounded-xl w-full md:px-0 md:w-11/12 my-2">
         <div className="mx-auto  ">
           <div className="w-full mx-auto text-left flex justify-center items-center pb-8 px-8 dark:bg-zinc-900 rounded-xl">
